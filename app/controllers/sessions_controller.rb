3c884ecdbaaf12ec6b7593817f161927ae8eeae7
Matthew Robinson <matt_robinson1@yahoo.com>
2019-11-20 18:33:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,23 @@
 class SessionsController < ApplicationController
-    def new 
 
+  def new
+
+  end
+
+  def create
+    if params[:name] == "" || params[:name] == nil
+      redirect_to login_path
+      #redirects to the login page if :name is nil
+      #redirects to login page if :name is empty
+    else
+      session[:name] = params[:name] #sets session[:name] if :name was given
+      redirect_to '/' #redirects to "/" if logged in
     end
+  end
 
-    def create 
-        if !params[:name] || params[:name].empty?
-            redirect_to '/login'
-        else
-            session[:name] = params[:name]
-        redirect_to '/'
-        end
-    end
-
-    def destroy
-        if session[:name]
-            session.delete(:name)
-        end
-    end
-
-
-
-end  
+  def destroy
+    session.delete :name #clears session[:name] if it was set
+    #leaves session[:name] nil if it was not set
+    redirect_to '/login'
+  end
+end
